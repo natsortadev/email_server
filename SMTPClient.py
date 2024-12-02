52db4b1061f115f593d88cdc38417aafa3c9481e
Natália <smachadonatalia@gmail.com>
2024-12-02 14:45:27 -0300

    removendo comentários desnecessários

--- SMTPClient.py
+++ SMTPClient.py
@@ -15,31 +15,19 @@ print('''
       ''')
 print("Bem-vindo(a) ao serviço de emails Sumpimpa.net!")
 
-def chooseServer():
-    serverOp = input("Qual servidor de email gostaria de utilizar?\n[1] - Gmail\n[2] - Yahoo\n> ")
-    if int(serverOp) == 1:
-        mailserver = 'smtp.gmail.com'
-    elif int(serverOp) == 2:
-        mailserver = 'smtp.mail.yahoo.com'
-    else:
-        print("Inserção inválida.\n")
-        mailserver = chooseServer()
-    return mailserver
-
 op = input("Gostaria de usar o serviço Ethereal para enviar sua mensagem ou seu próprio email pessoal?\n[1] - Usar email padrão\n[2] - Usar email pessoal\n> ")
 loop = True
 
-# Credentials
+# Credenciais
 while (loop):
     if int(op) == 1:
+        # Use as credenciais abaixo para acessar a caixa de entrada e saída do serviço de email Ethereal https://ethereal.email/messages
         sender = username = "theron.schimmel@ethereal.email"
         password = "3QUZbwtQv91gzZTmje"
         mailserver = 'smtp.ethereal.email'
         loop = False
     elif int(op) == 2:
-        # Choose a mail server (e.g. Google mail server) and call it mailserver
-        mailserver = chooseServer()
-        
+        mailserver = 'smtp.gmail.com'
         sender = username = input("Digite seu email:\n> ")
         password = input("Insira a senha de app associada ao email inserido previamente:\n(Siga o tutorial para obter sua senha de app: https://support.google.com/accounts/answer/185833?hl=pt-BR)\n> ") 
         loop = False
@@ -50,7 +38,7 @@ receiver = input("Digite o email do destinatário:\n> ")
 subject = input("Assunto:\n> ")
 body = input("Texto do email:\n> ")
 
-# Message to send
+# Mensagem a ser enviada
 msg = (
 	f"From: {sender}\r\n"
     f"To: {receiver}\r\n"
@@ -60,17 +48,16 @@ msg = (
 )
 endmsg = '\r\n.\r\n'
 
-# Create socket called clientSocket and establish a TCP connection with mailserver
+# Cria um socket e estabelece uma conexão TCP com o servidor de email
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
-# Port number may change according to the mail server
 clientSocket.connect((mailserver, 587))
 recv = clientSocket.recv(1024).decode()
 print(recv)
 if recv[:3] != '220':
 	print('220 reply not received from server.') ##
 
-# Send HELO command and print server response.
+# Envia comando HELO
 heloCommand = "HELO gmail.com\r\n"
 clientSocket.send(heloCommand.encode())
 recv1 = clientSocket.recv(1024).decode()
@@ -78,7 +65,7 @@ print(recv1)
 if recv1[:3] != '250':
 	print('250 reply not received from server.')
 
-# Send email
+# Envia o email
 starttlsCommand = "STARTTLS\r\n"
 clientSocket.send(starttlsCommand.encode())
 recv2 = clientSocket.recv(1024).decode()
@@ -115,7 +102,7 @@ secureSocket.send(mailfrom.encode())
 recv3d = secureSocket.recv(1024).decode()
 print(recv3d)
 if recv3d[:3] != '250':
-    print('250 reply not received from server.') ## aaa
+    print('250 reply not received from server.')
     
 rcptto = f"RCPT TO: <{receiver}>\r\n"
 secureSocket.send(rcptto.encode())
@@ -124,29 +111,30 @@ print(recv3e)
 if recv3e[:3] != '250':
     print('250 reply not received from server.')
 
-# Send DATA command and print server response. 
+# Envia o comando DATA 
 data = 'DATA\r\n'
 secureSocket.send(data.encode())
 recv4 = secureSocket.recv(1024).decode()
 print(recv4)
 if recv4[:3] != '354':
-	print('354 reply not received from server.') ##
+	print('354 reply not received from server.')
 
-# Send message data
+# Envia os dados da mensagem
 secureSocket.send(msg.encode())
 secureSocket.send(endmsg.encode())
 
 recv5 = secureSocket.recv(1024).decode(errors='replace')
-print(recv5) # Cannot Decode response
+print(recv5)
 if recv5[:3] != '250':
 	print('250 reply not received from server.')
 
-# Send QUIT command and get server response.
+# Envia o comando QUIT
 quitcommand = 'QUIT\r\n'
 secureSocket.send(quitcommand.encode())
 recv6 = secureSocket.recv(1024).decode()
 print(recv6) # Unrecognized command
 if recv6[:3] != '221':
-	print('221 reply not received from server.') ##
+	print('221 reply not received from server.')
 
+# Fecha a conexão
 secureSocket.close()
